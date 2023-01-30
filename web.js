9791d711674c9117f668fc75adaa1a068a77623e
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2023-01-30 17:55:30 +0900

    init

--- web.js
+++ web.js
@@ -9,14 +9,15 @@ app.use(express.json());
 const cors = require('cors');
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '/views')));
+app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('/', (res, req) => {
-  req.sendFile(path.join(__dirname, '/views/index.html'));
+  req.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
+/* 리액트에서 라우팅 설정한 파일 읽도록 설정 */
 app.get('*', (res, req) => {
-  req.sendFile(path.join(__dirname, '/views/index.html'));
+  req.sendFile(path.join(__dirname, '/build/index.html'));
 });
 
 http.listen(PORT, () => {
