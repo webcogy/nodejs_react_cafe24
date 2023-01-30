681eb456f96459310e38e7cd222f10b8f719e3c9
DAEHYUN KIM <32122611+webcogy@users.noreply.github.com>
2023-01-30 16:39:19 +0900

    asdf

--- web.js
+++ web.js
@@ -5,12 +5,21 @@ const app = express()
 app.use(express.json());
 var cors = require('cors');
 app.use(cors());
-
+/*
 app.use(express.static(path.join(__dirname, '/build')))
 
 app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '/build/index.html'))
 });
+*/
+
+
+app.set('views', __dirname + '/build');
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
 
 app.listen(8001, function(){
     console.log('listeing on 8001')
